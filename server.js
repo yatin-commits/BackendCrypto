28b3b42159d705df63ee764f06d1555a33f3a6c4
Yatin Sharma <sharmayatin0882@gmail.com>
2025-07-22 12:46:56 +0530

    aaa

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173', 
-    'https://cointrading.vercel.app/'
+    'https://cointrading.vercel.app'
   ],
   credentials: true
 }));
